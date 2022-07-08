441521a040fcae9ea0f8c1528c3b5e491cc61751
Christabell <dimples.charles@gmail.com>
2022-07-08 06:50:28 -0700

    1-isdigit.c

---
+++ 1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - function that verifies if a character is adigit or not
+ *
+ */
+int _isdigit(int c)
+{
+	if((c >= 48) && (c <= 57))
+		return (1);
+	    
+	    return (0);
+}
